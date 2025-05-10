84de8e3fb6019d875e6863acb7bae21c8da21361
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 17:52:49 +0100

    Starting

--- src/SimpleIOU.sol
+++ src/SimpleIOU.sol
@@ -8,7 +8,31 @@ contract SimpleIOU{
     mapping (address => bool) public registerdFriends;
     address[] public friendList;
     mapping(address => uint256)public balances;
+    mapping(address => mapping (address => uint256)) public debts;
 
+    constructor (){
+        owner = msg.sender;
+        registerdFriends[msg.sender] = true;
+        friendList.push(msg.sender);
+    }
+
+    modifier onlyOwner () {
+        require(owner == msg.sender, "only the owner can call this function");
+        _;
+    }
+
+    modifier onlyRegistered (){
+        require(registerdFriends[msg.sender], "Not registered");
+        _;
+    }
+
+    function addFriend (address _friend) public onlyOwner {
+        require(_friend != address(0), "Not a valid address");
+        require(!registerdFriends[_friend], "Registered");
+         registerdFriends[_friend] = true;
+        friendList.push(_friend);
+
+    }
 
 
 }
